bb7c9a0d96e99e33670bf3266a66ad8ef42b432c
Sam Turrell <sam.turrell@netsells.co.uk>
2016-04-17 18:35:31 +0100

    Removed name

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 module.exports = {
     install: function(Vue, options) {
         Vue.component('breadcrumbs', {
-            name:     'breadcrumbs',
             template: '<nav class="breadcrumbs" v-if="$route.matched.length"> <ul> <template v-for="(i, crumb) in $route.matched"> <template v-if="shouldShow(crumb)"> <li> <a v-link="{  name: crumb.handler.name,  params: crumb.params,  exact: true  }">{{ crumb.handler.breadcrumb }}</a> </li> </template> </template> </ul> </nav>',
             methods:  {
                 shouldShow: function(crumb) {
